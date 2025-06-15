21b1a538cdf66eb82675547fd545d5d8e6faf084
PandiSimatupang <pandikisaran@gmail.com>
2025-06-15 21:54:04 +0100

    add testing wordList quick test

--- hangman.py
+++ hangman.py
@@ -1,7 +1,20 @@
+import random
+
+#quick test
+wordsList = ['apple', 'banana', 'cranbarry']
+
+
+#global variable
+tries = 5
+word = random.choice(wordsList)
+
+
+
 
 def main():
-    print("Hello World")
-    pass
+    print(f"test: {word}")
+
+
 
 
 
